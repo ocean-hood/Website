714cca073626b31bd5082bd05fa1599acd68fc46
JosephRana11 <josephranamgr7@gmail.com>
2023-10-07 17:34:10 +0545

    third commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view , begineer_view , intermediate_view , expert_view , about_us_view , intermediate1_view , intermediate2_view , intermediate3_view , intermediate4_view , intermediate5_view , intermediate6_view , intermediate7_view
 
 urlpatterns = [
-  path('' , home_view , name='home')
-
+  path('' , home_view , name='home'),
+  path('begineer' , begineer_view , name='begineer'),
+  path('intermediate' , intermediate_view , name='intermediate'),
+  path('expert' , expert_view , name='advanced'),
+  path('about' , about_us_view , name='about'),
+  path('intermediate/1' , intermediate1_view , name='paper1'),
+  path('intermediate/2' , intermediate2_view , name='paper2'),
+  path('intermediate/3' , intermediate3_view , name='paper3'),
+  path('intermediate/4' , intermediate4_view , name='paper4'),
+  path('intermediate/5' , intermediate5_view , name='paper5'),
+  path('intermediate/6' , intermediate6_view , name='paper6'),
+  path('intermediate/7' , intermediate7_view , name='paper7'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,36 @@ from django.shortcuts import render
 
 def home_view(request):
   return render(request , 'home.html')
+
+def begineer_view(request):
+  return render(request , 'beginner.html')
+
+def intermediate_view(request):
+  return render(request , 'intermediate/intermediate.html')
+
+def expert_view(request):
+  return render(request , 'expert.html')
+
+def about_us_view(request):
+  return render(request , 'about.html')
+
+def intermediate1_view(request):
+  return render(request , 'intermediate/1.html')
+
+def intermediate2_view(request):
+  return render(request , 'intermediate/2.html')
+  
+def intermediate3_view(request):
+  return render(request , 'intermediate/3.html')
+
+def intermediate4_view(request):
+  return render(request , 'intermediate/4.html')
+
+def intermediate5_view(request):
+  return render(request , 'intermediate/5.html')
+
+def intermediate6_view(request):
+  return render(request , 'intermediate/6.html')
+
+def intermediate7_view(request):
+  return render(request , 'intermediate/7.html')
